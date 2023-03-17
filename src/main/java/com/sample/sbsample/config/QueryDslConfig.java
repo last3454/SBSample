13b5cfa51c7d842adfa4e2c285439dcfe6934406
choi young hwan <last3454@naver.com>
2023-03-17 11:19:43 +0900

    테스트1

--- src/main/java/com/sample/sbsample/config/QueryDslConfig.java
+++ src/main/java/com/sample/sbsample/config/QueryDslConfig.java
@@ -1,8 +1,13 @@
 package com.sample.sbsample.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QueryDslConfig {
 
+    @PersistenceContext
+    private EntityManager entityManager;
 }
